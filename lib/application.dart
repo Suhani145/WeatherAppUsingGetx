3b4f496883d92fc1c7e6bad33737f726aea0fc1c
dart practice <mahjabinsuhani663@gmail.com>
2025-12-21 22:16:59 +0600

    updated

--- lib/application.dart
+++ lib/application.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weatherappwithgetx/bindings/weather_binding.dart';
 import 'package:weatherappwithgetx/views/weather_home_view.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
           colorScheme: .fromSeed(seedColor: Colors.deepPurple),
     ),
+    initialBinding: WeatherBinding(),
     home: WeatherHomeView(),
     );
   }
--- lib/views/weather_home_view.dart
+++ lib/views/weather_home_view.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
-class WeatherHomeView extends StatelessWidget {
-  const WeatherHomeView({super.key});
+import 'package:get/get.dart';
+import 'package:weatherappwithgetx/controllers/weather_controller.dart';
 
+class WeatherHomeView extends StatelessWidget {
+  WeatherHomeView({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final WeatherController weatherController = Get.find<WeatherController>();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Current Weather')),
+
+      body: Center(
+        child: Obx(
+          (){
+              if (weatherController.isLoading.value){
+                return CircularProgressIndicator();
+              }
+                final weather = weatherController.weatherData.value;
+                if(weather == null){
+                  return Text("No Data found");
+                }
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(weather.cityName,
+                        style: TextStyle(fontSize: 24)),
+                    SizedBox(height: 8),
+                    Text(weather.temp, style: TextStyle(fontSize: 40)),
+                    SizedBox(height: 8),
+                    Text(weather.feelsLike, style: TextStyle(fontSize: 18)),
+                  ],
+                );
+          }
+        ),
+      ),
+    );
   }
 }
