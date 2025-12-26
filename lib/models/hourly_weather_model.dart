79ceee852ca2c89cb94ecc86563d45782458c175
dart practice <mahjabinsuhani663@gmail.com>
2025-12-26 23:42:35 +0600

    updated

---
+++ lib/models/hourly_weather_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+class HourlyWeatherModel {
+  String dtTxt;
+  double hourlyTemp;
+  String icon;
+
+  HourlyWeatherModel({
+    required this.dtTxt,
+    required this.hourlyTemp,
+    required this.icon,
+  });
+
+  factory HourlyWeatherModel.fromJson(Map<String, dynamic> json) {
+    return HourlyWeatherModel(
+      dtTxt: json['dt_txt'] as String,
+      hourlyTemp: (json['main']['temp'] as num).toDouble(),
+      icon: json['weather'][0]['icon'] as String,
+    );
+  }
+}
+
+List<HourlyWeatherModel> hourlyWeatherList(Map<String, dynamic> json) {
+  List<dynamic> parseList = json['list'] as List;
+  return parseList
+      .map((hourlyJson) => HourlyWeatherModel.fromJson(hourlyJson))
+      .toList();
+}
